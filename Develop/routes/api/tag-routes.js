83f54a127bca779d936ce21af218148167afb6cc
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2022-11-16 13:14:43 -0600

    adding required fields in the API files category-routes, product-routes, and tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,11 +6,32 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagsData = Tag.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(tagsData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const singleTagData = Tag.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+
+    if(!singleTagData) {
+      res.status(404).json( {message: 'No tag was found with the id. '});
+      return;
+    }
+    res.status(200).json(singleTagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
